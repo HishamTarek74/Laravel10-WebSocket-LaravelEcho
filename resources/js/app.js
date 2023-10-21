f08f3e29b2b075a34e53f2253e7e6820089ed4a4
heshamtarek7222 <heshamtarek.7222>
2023-10-21 16:59:13 +0300

    fist commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp } from 'vue';
+
+import HomeComponent from './components/HomeComponent.vue';
+
+createApp(HomeComponent).mount("#app")
+
---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+    <h1>
+        How To Install Vue 3 in Laravel 10 : Laravel SPA :)
+    </h1>
+</template>
+
+<script >
+    export default {
+        name: "Home Component",
+        data() {
+            return {}
+        },
+
+        mounted() {
+            window.Echo.channel('SendMessageChannel').listen('SendMessage', event => {
+                console.log(event);
+            });
+        },
+
+    }
+</script>
